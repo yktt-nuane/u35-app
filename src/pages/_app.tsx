0f6c4cd09c7f337058bde41311380f13fd2b6abc
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-12-21 12:54:09 +0900

    change app name to JICAP

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>U35</title>
+        <title>JICAP</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
