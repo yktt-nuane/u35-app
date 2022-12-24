4f940fcf4b1c5e9cd135e89e6cd643e229631703
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-12-24 11:10:20 +0900

    introduce google analytics

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,24 +1,19 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ''
+export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
 
 // IDが取得できない場合を想定する
-export const existsGaId = GA_TRACKING_ID !== ''
-
-// PVを測定する
-export const pageview = (path) => {
-  window.gtag('config', GA_TRACKING_ID, {
-    page_path: path,
+export const existsGaId = GA_ID !== ''
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url) => {
+  window.gtag('config', GA_ID, {
+    page_path: url,
   })
 }
 
-// GAイベントを発火させる
-export const event = ({ action, category, label, value = '' }) => {
-  if (!existsGaId) {
-    return
-  }
-
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
   window.gtag('event', action, {
     event_category: category,
-    event_label: JSON.stringify(label),
-    value,
+    event_label: label,
+    value: value,
   })
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,5 @@
+import { existsGaId, GA_ID } from '@/lib/gtag'
+
 import { createGetInitialProps } from '@mantine/next'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
@@ -11,6 +13,29 @@ export default class _Document extends Document {
       <Html>
         <Head>
           <script async src="https://platform.twitter.com/widgets.js" />
+          {existsGaId && (
+            <>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+                key="googletagmanager"
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                }}
+                key="dangerouslySetInnerHTML"
+              />
+            </>
+          )}
         </Head>
         <body>
           <Main />
